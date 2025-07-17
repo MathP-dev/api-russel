8c49f77cc7caa92d4c6a4fd42bde5569265c4afa
Mathieu P. <mathieu.paquier85@gmail.com>
2025-07-17 13:15:11 +0200

    Initial commit

---
+++ db/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { path } = require('../app');
+require('dotenv').config({path: './env/.env'});
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.URI_MONGO);
+    console.log('MongoDB connecté...');
+  } catch (err) {
+    console.error('Erreur de connexion à MongoDB :', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
